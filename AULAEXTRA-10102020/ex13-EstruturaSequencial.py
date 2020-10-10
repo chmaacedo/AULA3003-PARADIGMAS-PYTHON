f35873173070d2465b0fa8286a40910969c4cfcc
chmaacedo <70985405+chmaacedo@users.noreply.github.com>
2020-10-10 17:13:42 -0300

    Exemplo 13 OK

---
+++ AULAEXTRA-10102020/ex13-EstruturaSequencial.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 10:29:03 2020
+
+@author: chmaa
+"""
+
+altura=float(input("Insira a altura:"))
+homem=(72.7*altura) - 58
+mulher=(62.1*altura) - 44.7
+
+print("O Peso ideal para os homens é:", homem)
+print()
+print("O Peso ideal para as mulheres é:", mulher)
