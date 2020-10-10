edc79e4fd15e2a96aebb6e579ef53db4e216c8e7
chmaacedo <70985405+chmaacedo@users.noreply.github.com>
2020-10-10 10:22:09 -0300

    Segundo exercicio OK

---
+++ AULAEXTRA-10102020/ex2-EstruturaSequencial.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 10:10:32 2020
+
+@author: Carlos Henrique Macedo de Araujo
+"""
+
+a=int(input("Digite um número:"))
+print("O número digitado foi:", a)
