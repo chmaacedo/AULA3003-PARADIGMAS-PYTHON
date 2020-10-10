f99b44a9fc276ffbbb848ccff3d249e27a6e1364
chmaacedo <70985405+chmaacedo@users.noreply.github.com>
2020-10-10 10:24:57 -0300

    Terceiro exercicio OK

---
+++ AULAEXTRA-10102020/ex3-EstruturaSequencial.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 10:13:03 2020
+
+@author: chmaa
+"""
+
+a=int(input("Digite o número A:"))
+b=int(input("Digite o número B:"))
+c= a+b
+print()
+print("\nA soma de A + B é:", c)
