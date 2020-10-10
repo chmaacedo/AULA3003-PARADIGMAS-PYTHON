320e034128d7690748fc405de5b0d3b14a2ec667
chmaacedo <70985405+chmaacedo@users.noreply.github.com>
2020-10-10 17:13:36 -0300

    Exemplo 9 Ok

---
+++ AULAEXTRA-10102020/ex9-EstruturaSequencial.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 10:24:18 2020
+
+@author: chmaa
+"""
+
+fahr=float(input("Digite a temperatura em Fahrnheit:"))
+cel=5*((fahr-32)/9)
+
+print("Temperatura em Celsius: {:04.2f}".format(cel))
